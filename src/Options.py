51d6b4e694c11ba0d8e4ce9771c3a67524e04fe4
Pasi Luostarinen <pasi.luostarinen@serv.fi>
2018-11-02 20:28:06 +0200

    added options code from previous project

---
+++ src/Options.py
@@ -0,0 +1,35 @@
+from PyQt5.QtCore import QSettings
+import os
+from os.path import expanduser
+
+
+def getPath(file=None):
+    global dirPath
+    if file is None:
+        return dirPath
+
+    return os.path.join(dirPath, file)
+
+
+def get(key, default, *formatStr):
+    global settings
+    val = settings.value(key, default)
+    if len(formatStr) == 0:
+        return val
+    else:
+        return val.format(*formatStr)
+
+
+def set(key, value):
+    global settings
+    settings.setValue(key, value)
+    settings.sync()  # save the value
+
+
+home = expanduser("~")
+dirPath = os.path.join(home, "JoyBind")
+if not os.path.exists(dirPath):
+    os.makedirs(dirPath)
+
+filePath = getPath("JoyBind.ini")
+settings = QSettings(filePath, QSettings.IniFormat)
